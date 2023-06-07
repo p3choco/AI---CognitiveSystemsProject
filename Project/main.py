fe5bc37722e615a9d5715428463fb8515ed9f8f6
p3choco <62072811+p3choco@users.noreply.github.com>
2023-06-08 00:00:56 +0200

    First good wersion of video feedback page

--- Project/main.py
+++ Project/main.py
@@ -1,34 +1,31 @@
 from kivy.app import App
+from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.image import Image
 from kivy.clock import Clock
 from kivy.graphics.texture import Texture
-import cv2
-from kivy.modules import inspector
-from kivy.uix.floatlayout import FloatLayout
 from kivy.core.window import Window
+import cv2
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_eye.xml')
-smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_smile.xml')
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
 
 class Launch(GridLayout):
     def __init__(self, **kwargs):
         super(Launch, self).__init__(**kwargs)
         self.cols = 1
         self.capture = cv2.VideoCapture(0)
+        self.image = Image(allow_stretch=True)
+        self.sign = Label(size_hint_y= 0.3)
+        self.add_widget(self.image)
+        self.add_widget(self.sign)
 
         self.go(self)
 
-
     def go(self, thing):
-        self.image = Image()
         Clock.schedule_interval(self.update, 1.0 / 30.0)
-        self.add_widget(self.image)
 
     def update(self, dt):
-        # Pobieranie ramki wideo z kamery OpenCV
-
         ret, frame = self.capture.read()
 
         if ret:
@@ -36,11 +33,9 @@ class Launch(GridLayout):
             frame = self.detect(gray, frame)
             frame = cv2.flip(frame, 0)
 
-            # Konwersja ramki na teksturę Kivy
             texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
             texture.blit_buffer(frame.tobytes(), colorfmt='bgr', bufferfmt='ubyte')
 
-            # Ustawienie tekstury wideo w Kivy
             self.image.texture = texture
 
     def detect(self, gray, frame):
@@ -61,9 +56,14 @@ class Launch(GridLayout):
 
 class MainApp(App):
     def build(self):
+        self.title = 'Cognitive Project'
+        Window.size = (800, 585)
         launch = Launch()
-        inspector.create_inspector(Window, launch)
         return launch
 
-MainApp().run()
+    def on_stop(self):
+        launch = self.root
+        if launch.capture.isOpened():
+            launch.capture.release()
 
+MainApp().run()
