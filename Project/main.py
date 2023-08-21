9b62d72332a0d91a110d643dfffdfabcad45e4ae
p3choco <62072811+p3choco@users.noreply.github.com>
2023-08-21 19:00:22 +0200

    Graphical changes and small repairs done

--- Project/main.py
+++ Project/main.py
@@ -1,6 +1,10 @@
 import os
+import time
 from collections import deque
-from datetime import date
+from datetime import date, datetime, timedelta
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.label import Label
@@ -9,9 +13,10 @@ from kivy.uix.image import Image
 from kivy.clock import Clock
 from kivy.graphics.texture import Texture
 from kivy.core.window import Window
-import cv2
-import matplotlib.pyplot as plt
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.properties import ObjectProperty
+
+
 
 # Load the Haar cascade classifiers for face and smile detection
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
@@ -133,30 +138,9 @@ class Video(GridLayout):
         self.save_last_hour(run_smile_ratio.pop())
         run_smile_ratio = []
 
-        self.show_plot(self)
 
-    def show_plot(self, unused):
-        today = date.today()
-        file_name = "Days/" + today.strftime("%Y-%m-%d") + ".txt"
-        if os.path.isfile(file_name):
-            # If the file exists, read the ratio
-            with open(file_name, "r") as file:
-                lines = file.readlines()
-                if len(lines) >= 2:
-                    ratio = float(lines[0])
-        else:
-            print("No Data")
 
-        labels = ['Smile', 'Not Smile']
-        sizes = [ratio, 1 - ratio]
-        colors = ['orange', 'purple']
 
-        # Display the pie chart of smile vs not smile ratio
-        plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
-        plt.axis('equal')
-        plt.title('Ratio of Smile vs Not Smile')
-
-        plt.show()
 
     def save_last_hour(self, new_value):
         file_name = "Last_Hour" + ".txt"
@@ -179,56 +163,211 @@ class Video(GridLayout):
             for value in values:
                 file.write(str(value) + '\n')
 
-        hour_ratio = sum(values) / len(values)
-
-        labels = ['Smile', 'Not Smile']
-        sizes = [hour_ratio, 1 - hour_ratio]
-        colors = ['orange', 'purple']
-
-        # Display the pie chart of smile ratio for the last hour
-        plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
-        plt.axis('equal')
-        plt.title('Last Hour Smiles')
-
-        plt.show()
 
 class VideoWindow(Screen):
     def __init__(self, **kwargs):
         super(VideoWindow, self).__init__(**kwargs)
         self.layout = Video()
         self.add_widget(self.layout)
-        self.add_widget(Button(text="Show Stats", size_hint=(1, 0.1), pos_hint={'x': 0, 'y': 0}, on_press=self.go_to_second))
+        self.add_widget(Button(text="Show Stats",  background_color='#503C9A', background_normal='', size_hint=(0.3, 0.05), pos_hint={'x': 0.33, 'y': 0.025}, on_press=self.go_to_stats))
 
-    def go_to_second(self, instance):
+    def go_to_stats(self, instance):
         app = App.get_running_app()
         app.root.current = "stats"
 
 
 class StatsWindow(Screen):
+
+    btn = ObjectProperty(None)
     def __init__(self, **kwargs):
         super(StatsWindow, self).__init__(**kwargs)
-        self.add_widget(Button(text="Go to video", size_hint=(1, 0.1), pos_hint={'x': 0, 'y': 0}, on_press=self.go_to_first))
+        self.layout = GridLayout(cols=1)
+        self.add_widget(self.layout)
+        self.add_widget(Button(text="Go to video", background_color='#503C9A', background_normal='', size_hint=(0.3, 0.05), pos_hint={'x': 0.33, 'y': 0.025}, on_press=self.go_to_video))
+        self.display_all_plots(self)
+        Clock.schedule_interval(self.display_all_plots, 10)
+        self.size = [1000,800]
+        print(self.size)
 
-    def go_to_first(self, instance):
+    def do_last_hour_plot(self, unused):
+        # Read the smile ratios from the "Last_Hour.txt" file
+        smile_ratios = []
+
+        with open("Last_Hour.txt", "r") as file:
+            lines = file.readlines()
+            for line in lines:
+                smile_ratios.append(float(line))
+        print(smile_ratios)
+
+        ratio = sum(smile_ratios)/len(smile_ratios)
+        self.layout.clear_widgets()
+        if smile_ratios:
+            labels = ['Smile', 'Not Smile']
+            sizes = [ratio, 1 - ratio]
+            colors = ['#FFA500', 'purple']
+
+            # Create a pie chart and save it as an image file
+            plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
+            plt.axis('equal')
+            plt.title('Last Hour')
+
+    def do_day_plot(self, unused):
+        today = date.today()
+        ratio = 0
+        file_name = "Days/" + today.strftime("%Y-%m-%d") + ".txt"
+        if os.path.isfile(file_name):
+            # If the file exists, read the ratio
+            with open(file_name, "r") as file:
+                lines = file.readlines()
+                if len(lines) >= 2:
+                    ratio = float(lines[0])
+        else:
+            print("No Data")
+
+        labels = ['Smile', 'Not Smile']
+        sizes = [ratio, 1 - ratio]
+        colors = ['#FFA500', 'purple']
+
+        # Display the pie chart of smile vs not smile ratio
+        plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
+        plt.axis('equal')
+        plt.title('This Day')
+
+        # plt.show()
+
+            
+    def go_to_video(self, instance):
         app = App.get_running_app()
         app.root.current = "video"
 
+    def do_week_plot(self, unused):
+        from datetime import date, datetime, timedelta
+
+        # Get the path to the directory with data files
+        data_dir = 'Days/'
+
+        # Get today's date
+        today = date.today()
+
+        # Create lists to store data about the time spent in the app and the amount of smile time
+        app_time = []
+        smile_time = []
+
+        # Iterate over the last 7 days
+        for i in range(7):
+            # Calculate the date for the given day
+            date = today - timedelta(days=i)
+
+            # Create the path to the data file
+            file_path = os.path.join(data_dir, date.strftime("%Y-%m-%d") + ".txt")
+
+            # Check if the file exists
+            if os.path.isfile(file_path):
+                # Read data from the file
+                with open(file_path, "r") as file:
+                    lines = file.readlines()
+                    if len(lines) >= 2:
+                        # Get the time spent in the app
+                        time_in_app = float(lines[1].strip())
+                        app_time.append(time_in_app)
+
+                        # Get the smile time
+                        smile_ratio = float(lines[0].strip())
+                        smile_time.append(time_in_app * smile_ratio)
+                    else:
+                        # If the file exists but doesn't have enough lines, set the value to 0
+                        app_time.append(0)
+                        smile_time.append(0)
+            else:
+                # If the file doesn't exist, set the value to None
+                app_time.append(0)
+                smile_time.append(0)
+
+        # Reverse the order of the lists so the chart displays days in the correct order
+        app_time.reverse()
+        smile_time.reverse()
+
+        # Create a list of days
+        start_date = datetime.now() - timedelta(days=6)
+        days = []
+
+        for i in range(7):
+            current_date = start_date + timedelta(days=i)
+            day_name = current_date.strftime('%a')
+            days.append(day_name)
+
+        x = np.arange(len(app_time))  # Create an array of numbers from 0 to len(app_time)
+        width = 0.7  # Width of the bars
+
+        ax = plt.gca()
+
+        app_time_hours = [time / 120 for time in app_time]
+        smile_time_hours = [time / 120 for time in smile_time]
+
+        plt.bar(x, app_time_hours, width, label='App Time', color='#2077B4')
+        plt.bar(x, smile_time_hours, width, label='Smile Time', color='#FFA500')
+
+        plt.xlabel('Days')
+        plt.ylabel('Hours')
+        plt.title('App Time vs. Smile Time')
+
+        plt.xticks(x + width / 2, days)  # Set the x-axis labels to the appropriate dates
+
+        # Adjust the x-axis labels to fit the chart bars
+        plt.gca().set_xticklabels(days, rotation=45, ha='right')
+        plt.grid(axis='y', linestyle='--', linewidth=0.7, alpha=0.6)
+
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['bottom'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+
+        max_hours = 12
+        plt.ylim([0, max_hours])
+
+        plt.legend()
+
+    def display_all_plots(self, unused):
+        # Size of the main chart window
+        plt.figure(figsize=(15, 10))
+
+        # Chart from the last hour (top-left)
+        plt.subplot(2, 2, 1)
+        self.do_last_hour_plot(None)
+
+        # Daily chart (top-right)
+        plt.subplot(2, 2, 3)
+        self.do_day_plot(None)
+
+        # Weekly chart (bottom, full width)
+        plt.subplot(1, 2, 2)
+        self.do_week_plot(None)
+
+        plt.tight_layout()
+
+        timestamp = str(int(time.time()))
+        plt.savefig(timestamp + "chart.png")  # Save the chart as an image file
+
+        # Create an Image widget and set the source to the saved image file
+        image_widget = Image(source=timestamp + "chart.png", allow_stretch=True)
+        plt.clf()
+        os.remove(timestamp + "chart.png")
+
+        # Clear the layout and add the Image widget
+        self.layout.add_widget(image_widget)
 
 class WindowManager(ScreenManager):
     pass
 class MainApp(App):
     def build(self):
         self.title = 'Cognitive Project'
-        Window.size = (800, 585)
+        Window.size =  (1000, 800)
         manager = WindowManager()
+
         manager.add_widget(VideoWindow(name="video"))
         manager.add_widget(StatsWindow(name="stats"))
-
+        print(self.__sizeof__())
+        Window.clearcolor = (1, 1, 1, 1)
         return manager
-
-    def on_stop(self):
-        launch = self.root
-        if launch.capture.isOpened():
-            launch.capture.release()
 
 MainApp().run()
